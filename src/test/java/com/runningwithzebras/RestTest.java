63430efbfc14dd7c6b9c2028904985962b47dae7
Brian Northan <bnorthan@gmail.com>
2015-03-25 20:10:41 -0400

    Initial Commit

---
+++ src/test/java/com/runningwithzebras/RestTest.java
@@ -0,0 +1,72 @@
+package com.runningwithzebras;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import com.runningwithzebras.parsers.dom.ZebraListParser;
+import com.runningwithzebras.rest.ZebraApiConsumer;
+
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertSame;
+
+public class RestTest {
+
+	@Test
+	public void testRest() {
+		ZebraApiConsumer consumer = new ZebraApiConsumer();
+
+		InputStream stream;
+		try {
+			stream = consumer.getInputStream();
+
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder dBuilder;
+			Document doc;
+
+			dBuilder = dbFactory.newDocumentBuilder();
+			doc = dBuilder.parse(stream);
+
+			String root = doc.getDocumentElement().getNodeName();
+
+			assertEquals("response", root);
+		} catch (Exception ex) {
+			assert (false);
+		}
+	}
+
+	@Test
+	public void testZebraRest() {
+		ZebraApiConsumer consumer = new ZebraApiConsumer();
+
+		InputStream stream;
+		try {
+			stream = consumer.getInputStream();
+
+			ZebraListParser listParser = new ZebraListParser();
+
+			ArrayList<Zebra> zebraList = listParser.ObjectFromDOM(stream);
+
+			System.out.println("zebra size: " + zebraList.size());
+
+			for (Zebra zebra : zebraList) {
+				System.out.println("zeb: " + zebra.getDisplayTitle());
+			}
+
+		} catch (Exception ex) {
+			System.out.println("Exception!! " + ex);
+			assert (false);
+		}
+
+	}
+
+}
