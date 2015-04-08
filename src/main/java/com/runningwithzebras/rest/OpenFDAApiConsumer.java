24ca2ad119c750f79df3f22b452ccb799957a293
Brian Northan <bnorthan@gmail.com>
2015-04-08 08:44:37 -0400

    add more example api calls

--- src/main/java/com/runningwithzebras/rest/OpenFDAApiConsumer.java
+++ src/main/java/com/runningwithzebras/rest/OpenFDAApiConsumer.java
@@ -1,9 +1,22 @@
 package com.runningwithzebras.rest;
 
 public class OpenFDAApiConsumer extends AbstractRestApiConsumer {
-
+	
+	// some example API calls TODO: set api parameters with properties then build API string
+	
+	// get adverse events for Rett Syndrome, return exact event
+	String baseAPI3="https://api.fda.gov/drug/event.json?search=receivedate:[20040101+TO+20150101]+AND+patient.drug.drugindication:Rett+Syndrome&count=patient.reaction.reactionmeddrapt.exact";
+	
+	// get adverse events for hypotension drugs, return date of event
+	String baseAPI1="https://api.fda.gov/drug/event.json?search=receivedate:[20040101+TO+20150101]+AND+patient.drug.drugindication:hypertension&count=receivedate";
+	
+	// get adverse event for 'medicinalproduct' aspirin 
+	String baseAPI2="https://api.fda.gov/drug/event.json?search=patient.drug.medicinalproduct:aspirin&count=patient.reaction.reactionmeddrapt.exact";
+	
+	String baseAPI4 = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.pharm_class_epc:\"nonsteroidal+anti-inflammatory+drug\"&count=patient.reaction.reactionmeddrapt.exact";
+	
 	public OpenFDAApiConsumer() {
-		this.baseAPI = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.pharm_class_epc:\"nonsteroidal+anti-inflammatory+drug\"&count=patient.reaction.reactionmeddrapt.exact";
+		this.baseAPI = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.pharm_class_epc:nonsteroidal+anti-inflammatory+drug&count=patient.reaction.reactionmeddrapt.exact";
 	}
 
 	String createURL() {
